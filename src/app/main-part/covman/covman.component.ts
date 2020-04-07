05f7fa33c8d68d84daf775e4c1a4bc4f015bf9ab
munich99 <regerstrasse@icloud.com>
2020-04-07 11:34:18 +0200

    keydown - change direction of run-horizontal

--- src/app/main-part/covman/covman.component.ts
+++ src/app/main-part/covman/covman.component.ts
@@ -11,6 +11,7 @@ export class CovmanComponent implements OnInit {
 
   position:number = 40;  
   positionPx = this.position + "px";
+  positionDirection = "ArrowRight";
   
   innerWidth:number;
 
@@ -20,21 +21,30 @@ export class CovmanComponent implements OnInit {
 
    this.innerWidth = window.innerWidth;   
 
-   // interval(10).subscribe( () => this.move() );
+   interval(10).subscribe( () => this.move() );
 
    this._KeystrokeService.keyStroke$.subscribe(
-     messageKey =>{alert(messageKey)
-    });
-    
+     messageKey =>{
+       console.log(messageKey, "pacman richtung");
+       this.positionDirection = messageKey;
+    });    
   }
 
   move(){  
-    if(this.position <= (this.innerWidth-20)) {
-      this.position+= 2;      
-      this.positionPx = this.position + "px";
-    } else {
-      console.log("erreicht")
-    }
-   
+    if(this.position <= (this.innerWidth-20) && this.position >= 10)  {
+      switch (this.positionDirection) {
+        case "ArrowRight":
+          this.position+= 2;
+          this.positionPx = this.position + "px";
+          break;
+        case "ArrowLeft":
+          this.position-= 2;
+          this.positionPx = this.position + "px";
+          break;
+      }
+    }   
   }
+
+
+
 }
