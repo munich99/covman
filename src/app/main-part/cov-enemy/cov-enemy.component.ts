59d8ff7ddfbfd5246a1b0f2a25986cf4a335b8a7
munich99 <regerstrasse@icloud.com>
2020-04-24 17:20:52 +0200

    enemy is moving

--- src/app/main-part/cov-enemy/cov-enemy.component.ts
+++ src/app/main-part/cov-enemy/cov-enemy.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { PointServiceService } from '../../services/point-service.service';
 
 @Component({
@@ -8,18 +8,48 @@ import { PointServiceService } from '../../services/point-service.service';
 })
 export class CovEnemyComponent implements OnInit {
 
+  @ViewChild('enemy', {static: true}) enemyView:ElementRef;
+
+  positionx:number = 40; 
+  positiony:number = 20; 
+  positionX = this.positionx + "px";
+  positionY = this.positiony + "px";
+  positionDirection = "ArrowRight";
+
   constructor(public _PointServiceService:PointServiceService  ) { }
 
   ngOnInit() {
 
     this._PointServiceService.CovEnemyX$
     .subscribe(xx => {
-// console.log(xx, "xx");
+    // console.log(xx, "xx");
+      // this.moveEnemy()
     })
   }
 
   moveEnemy(){
-    
+    switch (this.positionDirection) {
+      case "ArrowRight":
+        this.positionx = this.enemyView.nativeElement.offsetLeft + 10; 
+        // this.movePermissionHorizotal = true;
+        break;
+      case "ArrowLeft":
+        this.positionx = this.enemyView.nativeElement.offsetLeft - 10; 
+        // this.movePermissionHorizotal = true;        
+        break;
+      case "ArrowUp":
+        this.positiony = this.enemyView.nativeElement.offsetTop - 10; 
+        // this.movePermissionHorizotal = false;         
+        break;
+      case "ArrowDown":
+        this.positiony = this.enemyView.nativeElement.offsetTop + 10; 
+        // this.movePermissionHorizotal = false;                   
+        break;
+    } 
+
+    this.positionX = this.positionx+  "px";
+    this.positionY = this.positiony+  "px";
+
   }
 
 }
