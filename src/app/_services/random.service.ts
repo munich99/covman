7029f2622b488ee8aaa9461afec2f848ba52b445
munich99 <regerstrasse@icloud.com>
2020-04-29 15:39:34 +0200

    lines are random multiple

--- src/app/_services/random.service.ts
+++ src/app/_services/random.service.ts
@@ -8,13 +8,13 @@ export class RandomService {
   PlaygroundWidth:number;
   PlaygroundHeight:number;
 
-  /*
-  dimensions = [
+ 
+  dimensions2 = [
     {xS:130, xW:10, yS:0, yH:110},
     {xS:200, xW:90, yS:130, yH:10},
     {xS:40, xW:360, yS:200, yH:10}
   ];
-  */
+
   
 
   constructor() { }
@@ -25,9 +25,19 @@ export class RandomService {
   }
 
   randomEngine(){  
-    let dimensions = this.randomEngineXY( (this.PlaygroundWidth/10), (this.PlaygroundHeight/10) );
-    console.log(dimensions,"zufi");
-    return dimensions;
+   
+    let preDimensions;
+    let Dimensions:object[]=[{xS:130, xW:10, yS:0, yH:110}];     
+    let i:number= 0; 
+    while(i <= 4){
+      preDimensions = this.randomEngineXY( (this.PlaygroundWidth/10), (this.PlaygroundHeight/10) );
+      Dimensions.push(preDimensions);
+      i++;
+    }
+   
+    preDimensions = this.randomEngineXY( (this.PlaygroundWidth/10), (this.PlaygroundHeight/10) );
+    console.log(Dimensions,"zufi");    
+    return Dimensions;
   }
 
   randomEngineXY(stopx:number, stopy:number){
@@ -43,9 +53,9 @@ export class RandomService {
     max = Math.floor( (stopy-1) );
     let randomValueStartD = (Math.floor(Math.random() * (max - min +1)) + min);
 
-    let randomValue:object = [
+    let randomValue:object = 
       {xS:(randomValueStart*10), xW:(randomValueEnd*10), yS:randomValueStartD, yH:10}
-    ]
+    
     return randomValue;    
   }
 
